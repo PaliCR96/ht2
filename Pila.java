3e16e5b0beac4932bf2871e4626eee9f38cfb5b3
PaliCR96 <can15053@uvg.edu.gt>
2016-07-25 17:56:50 -0600

    Metodos que se utilizaran para las pilas

---
+++ Pila.java
@@ -0,0 +1,41 @@
+/**
+Universidad del Valle de Guatemala
+Algoritmos y Estructura de datos
+Hoja de Trabajo 2
+Clase: Pila, clase principal que imprime resultado 
+ * @author Paulina Cano 15053
+ * @author Jackeline Hidalgo 15776
+ * @author Irvin Oliva 15816
+ **/
+
+import java.util.Vector;
+
+public class Pila <E> implements iPila<E> {
+	Vector<E> vector= new Vector<>();
+
+	@Override
+	public void push(E element) {
+		vector.addElement(element);
+	}
+
+	@Override
+	public E pop() {
+		return vector.lastElement();
+	}
+
+	@Override
+	public boolean IsEmpty() {
+		return vector.isEmpty();
+	}
+
+	@Override
+	public int Size() {
+		return vector.size();
+	}
+
+	@Override
+	public E get() {
+		return null;
+	}
+	
+}
