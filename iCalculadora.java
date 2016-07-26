a70920d7eb83f394675b32001a2b2bfa349dca30
hid15776 <hid15776@uvg.edu.gt>
2016-07-25 18:28:41 -0600

    Add files via upload
    Crear Interfaz de la calculadora

---
+++ iCalculadora.java
@@ -0,0 +1,16 @@
+/**
+Universidad del Valle de Guatemala
+Algoritmos y Estructura de datos
+Hoja de Trabajo 2
+Clase: iCalculadora, interfaz de la calculadora 
+ * @author Paulina Cano 15053
+ * @author Jackeline Hidalgo 15776
+ * @author Irvin Oliva 15816
+ **/
+public interface iCalculadora {
+	
+	public int Calcular (String vector);
+	public String toString();
+	public String LeerArchivo(String direccion);
+
+}
