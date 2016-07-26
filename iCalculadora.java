dbce314b022bac4d703ea3fb1bc889a7453ee6cb
hid15776 <hid15776@uvg.edu.gt>
2016-07-25 18:31:34 -0600

    Update iCalculadora.java

--- iCalculadora.java
+++ iCalculadora.java
@@ -5,7 +5,6 @@ Hoja de Trabajo 2
 Clase: iCalculadora, interfaz de la calculadora 
  * @author Paulina Cano 15053
  * @author Jackeline Hidalgo 15776
- * @author Irvin Oliva 15816
  **/
 public interface iCalculadora {
 	
